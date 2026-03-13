d465028a7717d6b0cc38e40c1cceb6b0da2313e1
kjwu338 <kjwu@ualberta.ca>
2026-03-13 10:32:18 -0600

    nmds + permanova code update

--- 1_scripts/biol499script4b_v2_multivariate.R
+++ 1_scripts/biol499script4b_v2_multivariate.R
@@ -9,10 +9,13 @@
 # NMDS is a visualization tool; shows shape of data
 # PERMANOVA: calculates actual distance between groups
 
+install.packages("pairwiseAdonis")
 library(dplyr)
 library(ggplot2)
 library(vegan)
 library(tidyr)
+library(ggnewscale)
+library(pairwiseAdonis) # pairwise PERMANOVA
 
 # STEP 0: Load and clean data
 gc <- read.csv("5_outputtables/gc_wcategories.csv")
@@ -34,10 +37,10 @@ gc_clean <- gc %>%
 # create species matrix wide format
 sp_wide <- gc_clean %>%
   # Note: Join by site/cond/depth
-  group_by(tray, site, microsite_condition, surface_depth, species) %>%
+  group_by(Tray.., site, microsite_condition, surface_depth, Species) %>%
   summarise(count = n(), .groups = "drop") %>%
-  filter(!is.na(species)) %>%  
-  pivot_wider(names_from = species, values_from = count, values_fill = 0) 
+  filter(!is.na(Species)) %>%  
+  pivot_wider(names_from = Species, values_from = count, values_fill = 0) 
 
 # Metadata and Matrix
 meta_perm <- sp_wide %>% 
@@ -70,19 +73,31 @@ nmds_scores <- as.data.frame(scores(nmds_gc, display = "sites"))
 plot_data <- cbind(nmds_scores, meta_perm)
 
 # colors
-cols <- c("biocrust" = "#8B9E6B", "dung" = "#C4944A", "control" = "#D4C09A")
+cols_microsite <- c("biocrust" = "#8B9E6B", "dung" = "#C4944A", "control" = "#D4C09A")
+cols_depth     <- c("surface" = "#4A90D9", "0-5" = "#E8A838", "5-10" = "#C76BB5")
 
 ggplot(plot_data, aes(x = NMDS1, y = NMDS2)) +
-  # add 95% Confidence Ellipses
+  # add 95% Confidence Ellipses for microsite
   stat_ellipse(aes(color = microsite_condition), type = "t", level = 0.95, linewidth = 1) +
+  scale_color_manual(values = cols_microsite, name = "Microsite") +
+  
+  # second color scale for points
+  new_scale_color() +
+  
+  #or for depth
+  stat_ellipse(aes(color = surface_depth), type = "t", level = 0.95, linewidth = 1) +
+  scale_color_manual(values = cols_depth, name = "Depth") +
+  
+  # third color scale for points
+  new_scale_color() +
   
   # add points + alpha makes them slightly transparent
   geom_point(aes(color = microsite_condition, shape = surface_depth), 
              size = 3, alpha = 0.7) +
   
   # color and shapes
-  scale_color_manual(values = cols, name = "Microsite") +
-  scale_shape_manual(values = c(16, 17, 15), name = "Depth") + # 16=circle, 17=tri, 15=square
+  scale_color_manual(values = cols_microsite, name = "Microsite") +
+  scale_shape_manual(values = c(16, 17, 15), name = "Depth") +
   
   # legend
   theme_bw() +
@@ -102,6 +117,7 @@ ggplot(plot_data, aes(x = NMDS1, y = NMDS2)) +
 # a. Beta dispersion: selective filtering 
 # is variation within dung different than biocrust?
 disp_micro <- betadisper(bray_dist, meta_perm$microsite_condition)
+disp_micro
 
 anova(disp_micro) # p < 0.05 means one microsite is more selective/constrained
 #notes: p value = 0.2353, no one microsite is more selective for species
@@ -112,10 +128,16 @@ permutest(disp_micro, pairwise = TRUE) # indicates which pair differs
 #make 8 treatment groups
 meta_perm$combo <- interaction(meta_perm$microsite_condition, meta_perm$surface_depth)
 
-# are species composition different between groups?
-perm_results <- adonis2(sp_mat_sqrt ~ combo, 
+# are species composition different between 8 groups?
+# if nmds ellipses are by microsite or depth, it would not accurate represent a permanova comparison
+# between 8 groups
+# cannot just go microsite_conditon * surface_depth bc control/surface missing
+perm_results <- adonis2(sp_mat_sqrt ~ combo,
                         data = meta_perm, 
                         strata = meta_perm$site, # Block by site x20
                         method = "bray")
 print(perm_results)
-
+# disp_microsite: dispersal between microsite is similar, but communities differ in composition between 8 groups
+# ~microsite condition: p = 0.013
+# ~combo: p = 0.001
+# ~ surface_depth p = 0.009
